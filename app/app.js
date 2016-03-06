b9d2323966f3adac6136783db4d1ac4f8611045a
Chris Gaona <thebeloved88@sbcglobal.net>
2016-03-06 09:47:39 -0800

    Created each module for todo.bundle.js with webpack...adds npm mongoose

--- app/app.js
+++ app/app.js
@@ -5,3 +5,6 @@ var angular = require('angular');
 angular.module('todoListApp', []);
 
 require('./scripts/controllers/main.js');
+require('./scripts/controllers/todo.js');
+require('./scripts/directives/todo.js');
+require('./scripts/services/data.js');
