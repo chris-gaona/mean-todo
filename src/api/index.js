8ed4cc8346d7fbf96d6de5f318901766174622dc
Chris Gaona <thebeloved88@sbcglobal.net>
2016-03-06 17:07:05 -0800

    Adds delete functionality to todo app

--- src/api/index.js
+++ src/api/index.js
@@ -44,5 +44,14 @@ router.put('/todos/:id', function(req, res) {
 });
 
 //TODO: ADD DELETE route to delete entries
+router.delete('/todos/:id', function(req, res) {
+    var id = req.params.id;
+    Todo.findByIdAndRemove(id, function(err, result) {
+       if(err) {
+           return res.status(500).json({err: err.message});
+       } 
+        res.json({message: 'Todo deleted'});
+    });
+});
 
 module.exports = router;
